a53a598549906fbaeff7f37239c2d952491f342a
amithraj <amithrajklm@gmail.com>
2021-12-11 16:41:52 +0530

    animation update in login page

--- src/components/SignInContainer.js
+++ src/components/SignInContainer.js
@@ -4,48 +4,49 @@ import SignInDesignImage from '../images/SignInPageDesign3.png';
 import SignUpDesignImage from '../images/SignInPageDesign2.png';
 import { signInPageHeader, signInPageDescription, signUpPageHeader, signUpPageDescription } from '../config';
 
-const SignInContainer = ({type="default", signInPage=true, changePage}) => {
-    const signInContainerClassName = 'sign-in-container-'+type;
-    return(
+
+const SignInContainer = ({ type = "default", signInPage = true, changePage, children }) => {
+    const signInContainerClassName = 'sign-in-container-' + type;
+    return (
         <div className={signInContainerClassName}>
             {signInPage &&
-                <div className={signInContainerClassName+'-sign-in-form'}>
-
+                <div className={signInContainerClassName + `-sign-in-form`}>
+                    {children}
                 </div>
             }
             {signInPage &&
-                <div className={signInContainerClassName+'-sign-in-design'}>
-                    <div className={signInContainerClassName+'-sign-in-design-header'}>
+                <div className={signInContainerClassName + `-sign-in-design`}>
+                    <div className={signInContainerClassName + '-sign-in-design-header'}>
                         {signInPageHeader}
                     </div>
-                    <div className={signInContainerClassName+'-sign-in-design-description'}>
+                    <div className={signInContainerClassName + '-sign-in-design-description'}>
                         {signInPageDescription}
                     </div>
-                    <AppButton 
-                        className={signInContainerClassName+'-sign-in-design-button'} 
-                        label='SIGN IN'
-                        onClick={()=>changePage()}/>
-                    <img className={signInContainerClassName+'-sign-in-design-img'} src={SignInDesignImage} alt={'img'}/>
+                    <AppButton
+                        className={signInContainerClassName + '-sign-in-design-button'}
+                        label='SIGN UP'
+                        onClick={() => changePage()} />
+                    <img className={signInContainerClassName + '-sign-in-design-img'} src={SignInDesignImage} alt={'img'} />
                 </div>
             }
             {!signInPage &&
-                <div className={signInContainerClassName+'-sign-up-design'}>
-                    <div className={signInContainerClassName+'-sign-up-design-header'}>
+                <div className={signInContainerClassName + '-sign-up-design'}>
+                    <div className={signInContainerClassName + '-sign-up-design-header'}>
                         {signUpPageHeader}
                     </div>
-                    <div className={signInContainerClassName+'-sign-up-design-description'}>
+                    <div className={signInContainerClassName + '-sign-up-design-description'}>
                         {signUpPageDescription}
                     </div>
-                    <AppButton 
-                        className={signInContainerClassName+'-sign-up-design-button'} 
-                        label='SIGN UP'
-                        onClick={()=>changePage()}/>
-                    <img className={signInContainerClassName+'-sign-up-design-img'} src={SignUpDesignImage} alt={'img'}/>
+                    <AppButton
+                        className={signInContainerClassName + '-sign-up-design-button'}
+                        label='SIGN IN'
+                        onClick={() => changePage()} />
+                    <img className={signInContainerClassName + '-sign-up-design-img'} src={SignUpDesignImage} alt={'img'} />
                 </div>
             }
             {!signInPage &&
-                <div className={signInContainerClassName+'-sign-up-form'}>
-
+                <div className={signInContainerClassName + '-sign-up-form'}>
+                    {children}
                 </div>
             }
         </div>
--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,14 +1,50 @@
 import './SignIn.scss'
 import SignInContainer from '../components/SignInContainer';
 import { useState } from 'react';
-
-const SignIn = ({type="default"}) =>{
-    const signInPageClassName = 'sign-in-'+type;
+import Form from 'react-bootstrap/Form';
+import AppButton from '../components/AppButton';
+const SignIn = ({ type = "default" }) => {
+    const signInPageClassName = 'sign-in-' + type;
     const [signInPage, setSignInPage] = useState(true);
 
-    return(
+    return (
         <div className={signInPageClassName}>
-            <SignInContainer signInPage={signInPage} changePage={()=>setSignInPage(!signInPage)}/>
+            <SignInContainer signInPage={signInPage} changePage={() => setSignInPage(!signInPage)}>
+                {signInPage &&
+                    <Form className={signInPageClassName + '-form'}>
+                        <div className={signInPageClassName + '-header'}>Sign In</div>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Control className={signInPageClassName + '-input'} type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Control className={signInPageClassName + '-input'} type="password" placeholder="Password" />
+                        </Form.Group>
+                        <AppButton label='LOGIN' />
+                    </Form>
+                }
+                {!signInPage &&
+                    <Form className={signInPageClassName + '-form'}>
+                        <div className={signInPageClassName + '-header'}>Sign Up</div>
+                        <Form.Group className="mb-3" controlId="formBasicUsername">
+                            <Form.Control className={signInPageClassName + '-input'} type="text" placeholder="Username" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Control className={signInPageClassName + '-input'} type="email" placeholder="Email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Control className={signInPageClassName + '-input'} type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Control className={signInPageClassName + '-input'} type="password" placeholder="Confirm Password" />
+                        </Form.Group>
+                        <AppButton label='SIGN UP' />
+                    </Form>
+                }
+            </SignInContainer>
         </div>
     )
 }
