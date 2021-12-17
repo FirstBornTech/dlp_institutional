37e729bd0c57c901510f905931fbf5c1478ed201
amithraj <amithrajklm@gmail.com>
2021-12-17 18:08:36 +0530

    Sidebar subcontent Ui design

--- src/components/AppBodySidebar.js
+++ src/components/AppBodySidebar.js
@@ -22,7 +22,79 @@ const AppBodySidebar = ({ type = "default" }) => {
         {
             key: 4,
             title: 'Writing',
-            path: '/Writing'
+            path: '/Writing',
+            content: [
+                {
+                    key: 1,
+                    title: 'Introduction'
+                },
+                {
+                    key: 2,
+                    title: 'Understanding questions ',
+                    subcontent: [
+                        {
+                            key: 1,
+                            title: 'Vistiqulum'
+                        },
+                        {
+                            key: 2,
+                            title: 'Triqulum'
+                        },
+                        {
+                            key: 3,
+                            title: 'Aliquam'
+                        }
+                    ]
+                },
+                {
+                    key: 3,
+                    title: 'Identify Keywords',
+                    subcontent: [
+                        {
+                            key: 1,
+                            title: 'Vistiqulum'
+                        },
+                        {
+                            key: 2,
+                            title: 'Triqulum'
+                        }
+                    ]
+                },
+                {
+                    key: 4,
+                    title: 'The six questions',
+                    subcontent: [
+                        {
+                            key: 1,
+                            title: 'One'
+                        },
+                        {
+                            key: 2,
+                            title: 'Two'
+                        },
+                        {
+                            key: 3,
+                            title: 'Three'
+                        },
+                        {
+                            key: 4,
+                            title: 'Four'
+                        },
+                        {
+                            key: 5,
+                            title: 'Five'
+                        },
+                        {
+                            key: 6,
+                            title: 'Six'
+                        }
+                    ]
+                },
+                {
+                    key: 5,
+                    title: 'Categorise case note'
+                },
+            ],
         },
         {
             key: 5,
@@ -47,16 +119,60 @@ const AppBodySidebar = ({ type = "default" }) => {
     ]
 
     const [selectedItem, setSelectedItem] = useState(0);
+    const [selectedItemContent, setSelectedItemContent] = useState(0);
+    const [selectedItemSubcontent, setSelectedItemSubcontent] = useState(0);
+
+    const renderSidebarDataSubcontent = (key, content) => {
+        return content.map((data, index) => {
+            return (
+                <>
+                    <div
+                        key={index}
+                        className={selectedItemSubcontent === index ? sidebarClassName + '-subcontent-selected' : sidebarClassName + '-subcontent-unselected'}
+                        onClick={() => setSelectedItemSubcontent(index)}>
+                        {key}.{data.key}.{data.title}
+                    </div>
+                </>
+            )
+        })
+    }
+
+    const renderSidebarDataContent = (content) => {
+        return content.map((data, index) => {
+            return (
+                <>
+                    <div
+                        key={index}
+                        className={(selectedItemContent === index && !data?.subcontent) ? sidebarClassName + '-content-selected' : sidebarClassName + '-content-unselected'}
+                        onClick={() => { setSelectedItemContent(index); setSelectedItemSubcontent(0) }}>
+                        {data.key}.{data.title}
+                    </div>
+                    {(selectedItemContent === index && data?.subcontent) &&
+                        <div className={sidebarClassName + '-container-subcontent'}>
+                            {renderSidebarDataSubcontent(data.key, data.subcontent)}
+                        </div>}
+                </>
+            )
+        })
+    }
 
     const renderSidebarData = () => {
         return sidebarData.map((data, index) => {
             return (
-                <div
-                    key={index}
-                    className={selectedItem === index ? sidebarClassName + '-selected' : sidebarClassName + '-unselected'}
-                    onClick={() => setSelectedItem(index)}>
-                    <Link to={data.path} className={sidebarClassName+'-link'}>{data.title}</Link>
-                </div>
+                <>
+                    <Link to={data.path} className={sidebarClassName + '-link'}>
+                        <div
+                            key={index}
+                            className={selectedItem === index ? sidebarClassName + '-selected' : sidebarClassName + '-unselected'}
+                            onClick={() => { setSelectedItem(index); setSelectedItemContent(0); setSelectedItemSubcontent(0) }}>
+                            {data.title}
+                        </div>
+                    </Link>
+                    {(selectedItem === index && data?.content) &&
+                        <div className={sidebarClassName + '-container-subcontent'}>
+                            {renderSidebarDataContent(data.content)}
+                        </div>}
+                </>
             )
         })
     }
