ae57126542ff137d07d9c47822d0f7d1ade227ca
amithraj <amithrajklm@gmail.com>
2021-12-28 19:09:00 +0530

    Static Introduction Page

--- src/views/Introduction.js
+++ src/views/Introduction.js
@@ -1,7 +1,43 @@
 const Introduction = () => {
     return (
-        <div style={{display:'flex', flexDirection:'column', margin:'100px 40px', color:'black'}}>
-            <h1>Introduction page is in progress !</h1>
+        <div style={{display:'flex', flexDirection:'column', margin:'120px 40px', color:'black'}}>
+            <h1>Welcome to Course</h1>
+            <p>
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudinMorbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                </p>
+                <br />
+                <h3>Contents Of Course</h3>
+                <ul>
+                    <li>Content</li>
+                    <li>Morbi</li>
+                    <li>Vestibulum</li>
+                    <li>Aliquam</li>
+                    <li>Sodaleseget</li>
+                </ul>
+                <br/>
+                <p>
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudinMorbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                </p>
         </div>
     )
 }
