bdce2506f87ac8491d6fe8d432528560ce3061eb
amithraj <amithrajklm@gmail.com>
2021-12-28 19:28:09 +0530

    Subject Static Data

--- src/views/Subject.js
+++ src/views/Subject.js
@@ -13,7 +13,43 @@ const Subject = ({ type = 'default', selectedContentType, changeContentType }) =
                 <AppCard type={`${selectedContentType !== 2 ? 'secondary' : 'default'}`} contentType={contentType[2]} onCardClick={() => changeContentType(2)} />
             </div>
             <div className={homeClassName + '__content'}>
-                <AppButton className={homeClassName + '__content-button'} label="Click here to move to next module"></AppButton>
+                <h1>Course Content</h1>
+                <p>
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudinMorbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                </p>
+                <br />
+                <div style={{textAlign:'center'}}>
+                <iframe width="420" height="315" title="youtube" 
+                    src="https://www.youtube.com/embed/6ncL7kUFBsI">
+                </iframe>
+                </div>
+                <br/>
+                <p>
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                    Morbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudinMorbi dictum. Vestibulum adipiscing
+                    pulvinar quam. In aliquam rhoncus sem.
+                    In mi erat, sodaleseget, pretium interdum,
+                    malesuada ac, augue. Aliquam sollicitudin
+                </p>
+            </div>
+            <div className={homeClassName + '__btn-section'}>
+                <AppButton className={homeClassName + '__btn-section-button'} label="Click here to move to next module"></AppButton>
             </div>
         </div>
     )
